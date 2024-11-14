b4f947823ec15dc739cb50cea30af8863c1d7a05
Dmitriy_Demin <d.demin@inbox.ru>
2024-11-14 22:55:01 +0400

    up

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,15 +3,15 @@ import java.beans.Introspector;
 public class Main {
     public static void main(String[] args) {
         int[] arr = {0, 1, 3, 5, 0, 0};
-        System.out.println(ReturnCountEvent(arr));
-        System.out.println(ReturnDifferentMinMax(arr));
-        System.out.println(IsTrue(arr));
+        System.out.println(returnCountEvent(arr));
+        System.out.println(returnDifferentMinMax(arr));
+        System.out.println(isTrue(arr));
 
     }
     /*
     * Написать метод, возвращающий количество чётных элементов массива. countEvens([2, 1, 2, 3, 4]) → 3
     * countEvens([2, 2, 0]) → 3 countEvens([1, 3, 5]) → 0*/
-    public static int ReturnCountEvent(int[] inputArr){
+    public static int returnCountEvent(int[] inputArr){
         int countEvent = 0;
         for (int element: inputArr){
             if (element%2 == 0) countEvent++;
@@ -21,7 +21,7 @@ public class Main {
 
     /*
     * Написать функцию, возвращающую разницу между самым большим и самым маленьким элементами переданного не пустого массива.*/
-    public static int ReturnDifferentMinMax (int [] inputArr){
+    public static int returnDifferentMinMax (int [] inputArr){
         int differentMinMax = 0;
         int min = inputArr[0];
         int max = inputArr[0];
@@ -37,7 +37,7 @@ public class Main {
     * Написать функцию, возвращающую истину, если в переданном массиве есть два соседних элемента, с нулевым значением.
     * */
 
-    public static boolean IsTrue (int [] inputArr){
+    public static boolean isTrue (int [] inputArr){
         boolean flag = false;
         for (int i = 0; i < inputArr.length-1; i++){
             if (inputArr[i] == 0 & inputArr[i+1] ==0)
